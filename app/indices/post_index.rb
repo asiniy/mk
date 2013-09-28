ef82a27ae38b18c00c4e548b579bf738bf3d2a18
Alex Antonov <admin@asiniy.com>
2013-09-28 23:00:37 +0400

    heroku deploting

--- app/indices/post_index.rb
+++ app/indices/post_index.rb
@@ -1,11 +1,13 @@
-ThinkingSphinx::Index.define :post, with: :active_record do
-  # fields
-  indexes heading
-  indexes short_description
-  indexes body
+if defined?(ThinkingSphinx)
+  ThinkingSphinx::Index.define :post, with: :active_record do
+    # fields
+    indexes heading
+    indexes short_description
+    indexes body
 
-  # attributes
-  has published
-  has categories(:id), as: :category_ids
-  has taggings(:tag_id), as: :tag_ids
+    # attributes
+    has published
+    has categories(:id), as: :category_ids
+    has taggings(:tag_id), as: :tag_ids
+  end
 end
